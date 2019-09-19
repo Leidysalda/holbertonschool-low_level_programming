e3a21ca431a9865c1d603cc7b125c24f3e573ccb
Leidysalda <leidysalda1@gmail.com>
2019-09-19 18:42:51 +0000

    First

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,6 +17,7 @@ printf("%d is positive\n", n); }
 if (n < 0)
 {
 printf("%d is negative\n", n); }
-else
-printf("%d is zero\n", n);
+else if (n == 0)
+{
+printf("%d is zero\n", n); }
 return (0); }
