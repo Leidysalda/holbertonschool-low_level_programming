832250905098208769342f78d18f61a676c08df5
Leidysalda <leidysalda1@gmail.com>
2019-09-20 00:03:28 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Numbersz
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n = 0;
+for (n = '0' ; n <= '9' ; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
