708dc2f15b06d10b53d0bad60a0f269c3edb6d57
Leidy J <44031927+Leidysalda@users.noreply.github.com>
2019-09-22 09:23:31 -0500

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
-* main - 
-*
-* Return: Always 0 (Success)
-*/
 int main(void)
 {
-int n;
-return ();
+int n = 0;
+char i = 'a';
+for (n = '0' ; n <= '9' ; n++)
+{
+putchar(n);
+}
+while (i <= 'f'){
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
 }
