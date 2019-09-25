90ced930cc100f961226ee4cd676ee3a1303692f
Leidysalda <leidysalda1@gmail.com>
2019-09-25 20:57:24 +0000

    Sing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,23 @@
 #include "holberton.h"
 /**
-* print_sign - print sign of a number
-*@n: The caracter to print
-*
-*Return: 0n success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+ * print_sign - print sign of a number
+ *@n: The caracter to print
+ *
+ *Return: 0n success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int print_sign(int n)
 {
-if (n > '0')
+if (n >= 0)
+{
 _putchar('+');
-_putchar('1');
-if
-(n == '0')
-_putchar('0');
-if
-(n < '0')
-_putchar('-');
-_putchar('1');
+return (1);
+}
+else if (n == 0)
+{
 return (0);
+}
+else
+_putchar('-');
+return (-1);
 }
