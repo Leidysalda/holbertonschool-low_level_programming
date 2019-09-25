36b0c5c56730482ffc5a8fe5d8c47d311780e4d3
Leidysalda <leidysalda1@gmail.com>
2019-09-25 20:37:15 +0000

    Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,15 @@
 #include "holberton.h"
 /**
-* _islower - checks for lowercase
-*@c: The caracter to print
-*
-*Return: 0n success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+ * _isalpha - checks for alphabetic character
+ *@c: The caracter to print
+ *
+ *Return: 0n success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _isalpha(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-_putchar('1');
-}
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
 return (0);
 }
