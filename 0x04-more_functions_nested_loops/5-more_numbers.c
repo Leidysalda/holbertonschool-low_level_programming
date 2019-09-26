cbe49fddecc77e5be8cbbb425627260298851601
Leidysalda <leidysalda1@gmail.com>
2019-09-26 18:47:59 +0000

    Merge

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * more_numbers - writes x 10 times
+ * @n: The caracter to print
+ *
+ *Return: 0
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void more_numbers(void)
+{
+   for (n = 48 ; n <= 57 ; n++)
+      {
+	_putchar(n);
+      }
+
+     _putchar('\n');
+  } while (i <= 13);
+}
