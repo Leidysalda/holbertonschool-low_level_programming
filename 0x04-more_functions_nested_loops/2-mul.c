409d87f36f46edea6bd134135c0daa17337ffa75
Leidysalda <leidysalda1@gmail.com>
2019-09-26 15:38:44 +0000

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *mul- multiplies two integers
+ *@a: integer
+ *@b: integer
+ *Return: 0n success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+int c = a * b;
+return (c);
+}
