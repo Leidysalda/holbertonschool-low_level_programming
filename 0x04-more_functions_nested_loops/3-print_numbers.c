ca57289764e89947ef4985c7e7a297a4e16c799f
Leidysalda <leidysalda1@gmail.com>
2019-09-27 00:59:55 +0000

    Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- *print_numbers(void)- print numbers
- *@a: Res
+ * print_numbers - print numbers
+ * Res
  *Return: 0n success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -9,8 +9,6 @@ void print_numbers(void)
 {
 int c;
 for (c = '0' ; c <= '9' ; c++)
-{
 _putchar(c);
-}
 _putchar('\n');
 }
