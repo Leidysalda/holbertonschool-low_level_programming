3106f0d6b343e74d871e9e6e3b0b4aa562857c64
Leidysalda <leidysalda1@gmail.com>
2019-09-27 01:15:50 +0000

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
  * more_numbers - writes x 10 times
- * : The caracter to print
+ * The caracter to print
  *
- *Return: 0
+ * Return: 0
  * On error, -1 is returned, and errno is set appropriately.
  */
 void more_numbers(void)
@@ -17,10 +17,10 @@ if (n > 9)
 {
 _putchar(n / 10 + '0');
 }
-_putchar(n % 10 +'0');
+_putchar(n % 10 + '0');
 }
 i++;
 _putchar('\n');
 }
-while (i <= 13);
+ while (i <= 9);
 }
