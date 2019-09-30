62b68c6de02ad412213ba88b3aae475c9bf159db
Leidysalda <leidysalda1@gmail.com>
2019-09-30 21:15:55 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - updates the value
+ *@n: The caracter to print
+ *
+ *Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,26 @@
+#ifndef RESET_TO_98_H
+#define RESET_TO_98_H
+void reset_to_98(int *n);
+#endif
+
 #ifndef _PUTCHAR
 #define _PUTCHAR
 int _putchar(char c);
+#endif
+
+#ifndef PRINT_ALPHABET
+#define PRINT_ALPHABET
+void print_alphabet(void);
+#endif
+
+#ifndef PRINT_ALPHABET_x10
+#define PRINT_ALPHABET_x10
+void print_alphabet_x10(void);
+#endif
+
+#ifndef _ISLOWER
+#define _ISLOWER
+int _islower(int c);
 #endif
 
 #ifndef PRINT_ALPHABET
