c786f61e41b4006375d86718eed9c14d416eb52a
Leidysalda <leidysalda1@gmail.com>
2019-10-01 15:54:44 +0000

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,13 @@
+#include <string.h>
 #include "holberton.h"
 /**
  * int _strlen - return the length of a string
  *@s: The caracter to print
- * 
+ *
  *Return: Always 0.
  */
 int _strlen(char *s)
 {
-
+char str[] = {"Holberton!"};
+_putchar(strlen(str))
 }
