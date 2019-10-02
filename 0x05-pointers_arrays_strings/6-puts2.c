1a1fea96e817ce031568ef2d6d15ea569a92a295
Leidysalda <leidysalda1@gmail.com>
2019-10-02 21:21:09 +0000

    Print every other character of a stringuts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,19 @@
  *
  *Return: Always 0.
  */
+
 void puts2(char *str)
 {
 int i;
+int j;
 i = 0;
 while (str[i] != '\0')
+i++;
+j = 0;
+while (j < i)
 {
-_putchar(str[i]);
-i += 2;
+putchar(str[j]);
+j += 2;
 }
-_putchar('\n');
+putchar ('\n');
 }
