3964c8d45009ae448417fe2fed0806f34d43a210
Leidysalda <leidysalda1@gmail.com>
2019-10-02 20:36:29 +0000

    Print every other character of a stringuts

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,74 +1,21 @@
 #ifndef PRINT_REV_H
 #define PRINT_REV_H
+
+void puts2(char *str);
+void rev_string(char *s);
 void print_rev(char *s);
-#endif
-
-#ifndef _STRLEN_H
-#define _STRLEN_H
 int _strlen(char *s);
-#endif
-
-#ifndef SWAP_INT_H
-#define SWAP_INT_H
 void swap_int(int *a, int *b);
-#endif
-
-#ifndef RESET_TO_98_H
-#define RESET_TO_98_H
 void reset_to_98(int *n);
-#endif
-
-#ifndef _PUTCHAR
-#define _PUTCHAR
 int _putchar(char c);
-#endif
-
-#ifndef PRINT_ALPHABET
-#define PRINT_ALPHABET
 void print_alphabet(void);
-#endif
-
-#ifndef PRINT_ALPHABET_x10
-#define PRINT_ALPHABET_x10
 void print_alphabet_x10(void);
-#endif
-
-#ifndef _ISLOWER
-#define _ISLOWER
 int _islower(int c);
-#endif
-
-#ifndef PRINT_ALPHABET
-#define PRINT_ALPHABET
 void print_alphabet(void);
-#endif
-
-#ifndef PRINT_ALPHABET_x10
-#define PRINT_ALPHABET_x10
 void print_alphabet_x10(void);
-#endif
-
-#ifndef _ISLOWER
-#define _ISLOWER
 int _islower(int c);
-#endif
-
-#ifndef _ISALPHA
-#define _ISALPHA
 int _isalpha(int c);
-#endif
-
-#ifndef PRINT_SING
-#define PRINT_SING
 int print_sign(int n);
-#endif
-
-#ifndef _ABS
-#define _ABS
 int _abs(int);
-#endif
-
-#ifndef PRINT_LAST_DIGIT
-#define PRINT_LAST_DIGIT
 int print_last_digit(int);
 #endif
