76c52c349eaefaf6f6de34a1b044eb6d964e5fc5
Leidysalda <leidysalda1@gmail.com>
2019-10-02 22:24:44 +0000

    Prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * puts_half - print a string in the midle
+ * @str: The caracter to print
+ *
+ *Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+int i;
+i = 0;
+while (str[i] != '\0')
+i++;
+if (i % 2 != 0)
+i++;
+i = i / 2;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}
