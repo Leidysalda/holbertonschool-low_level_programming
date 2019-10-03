f492235f9044d76912f99f4cc3d23a4dfa82fbb0
Leidysalda <leidysalda1@gmail.com>
2019-10-03 18:40:33 +0000

    Concaten

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strcat - print a string
+ * @dest: The caracter to print
+ * @src: The word to print
+ *Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+j = 0;
+}
+while (src[j] != '\0')
+{
+dest[i + j] = src[j];
+j++;
+}
+i++;
+ return(dest);
+}
+
