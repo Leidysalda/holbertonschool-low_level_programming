aaedd80f854180c2e206cc4c54d9a07e84c1072b
Leidysalda <leidysalda1@gmail.com>
2019-10-03 21:35:41 +0000

    Up

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strncpy - print a string
+ * @dest: The caracter to print
+ * @src: The word to print
+ * @n: account
+ *Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+for (i = 0 ; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for (; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
