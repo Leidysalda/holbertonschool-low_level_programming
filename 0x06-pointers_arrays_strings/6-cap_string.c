1113c6fd53188f8e0d2d3a590db217cb20aa0246
Leidysalda <leidysalda1@gmail.com>
2019-10-04 21:35:52 +0000

    Especial caracter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * *cap_string - print a string upper in especial caracter
+ * @s: The caracter to print
+ *
+ *Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == ','
+		    || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?'
+		    || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{'
+		    || s[i] == '}')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] -= 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
