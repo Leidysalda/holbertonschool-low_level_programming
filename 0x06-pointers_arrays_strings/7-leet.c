753c86429885648a6ca9c29156302b0e6b27e2c3
Leidysalda <leidysalda1@gmail.com>
2019-10-05 03:59:55 +0000

    Replace

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * *leet - print a string upper in especial caracter
+ * @s: The caracter to print
+ *
+ *Return: Always 0.
+ */
+char *leet(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		else if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		else if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		else if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		else if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+		i++;
+	}
+	return (s);
+}
