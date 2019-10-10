05079bd3fe75abde749d6bc948debe9bd6beba3b
Leidysalda <leidysalda1@gmail.com>
2019-10-10 18:08:12 +0000

    Chessboard

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,8 @@
 #ifndef PRINT_REV_H
 #define PRINT_REV_H
 
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
