04f230cf4b2f568ddbb5b92f5a1ad90e14f18650
Leidysalda <leidysalda1@gmail.com>
2019-10-10 17:52:57 +0000

    Palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -2,49 +2,50 @@
 #include "holberton.h"
 /**
  * _strlen - a function long.
- * @s: char 
+ * @s: char
  *Return: Always 0.
  */
 int _strlen(char *s)
 {
-      if (*s != '\0')
-      {
-            return (1 + _strlen(s + 1));
-      }
-      return (0);
+	if (*s != '\0')
+	{
+		return (1 + _strlen(s + 1));
+	}
+	return (0);
 }
 
 /**
- * aux - a function 
+ * aux - a function
  * @s: base
  * @x: number
  *Return: Always 0.
  */
 int aux(char *s, int x)
 {
-      if (x <= 0)
-      {
-            return (1);
-      }
-      if (*s == *(s + (x - 1)))
-      {
-            return (aux(s + 1, x - 2));
-      }
-      else
-      {
-            return(0);
-      }
+	if (x <= 0)
+	{
+		return (1);
+	}
+	if (*s == *(s + (x - 1)))
+	{
+		return (aux(s + 1, x - 2));
+	}
+	else
+	{
+		return (0);
+	}
 }
 
 /**
- * is_palindrome - a function 
+ * is_palindrome - a function
  * @s: base
- * 
+ *
  *Return: Always 0.
  */
 int is_palindrome(char *s)
 {
-      int l;
-      l = _strlen(s);
-      return (aux(s, l));
+	int l;
+
+	l = _strlen(s);
+	return (aux(s, l));
 }
