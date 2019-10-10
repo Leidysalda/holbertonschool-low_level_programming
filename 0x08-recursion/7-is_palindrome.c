699cfc931307dddeaa8794d39ed91bf036651480
Leidy J <44031927+Leidysalda@users.noreply.github.com>
2019-10-09 23:34:20 -0500

    Update 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -9,7 +9,7 @@ int _strlen(char *s)
 {
       if (*s != '\0')
       {
-              return (1 + _strlen(s + 1));
+            return (1 + _strlen(s + 1));
       }
       return (0);
 }
@@ -22,18 +22,18 @@ int _strlen(char *s)
  */
 int aux(char *s, int x)
 {
-        if (x <= 0)
-        {
-                return (1);
-        }
-        if (*s == *(s + (x - 1)))
-        {
-                return (aux(s + 1, x - 2));
-        }
-        else
-        {
-                return(0);
-        }
+      if (x <= 0)
+      {
+            return (1);
+      }
+      if (*s == *(s + (x - 1)))
+      {
+            return (aux(s + 1, x - 2));
+      }
+      else
+      {
+            return(0);
+      }
 }
 
 /**
@@ -44,7 +44,7 @@ int aux(char *s, int x)
  */
 int is_palindrome(char *s)
 {
-        int l;
-        l = _strlen(s);
-        return (aux(s, l));
+      int l;
+      l = _strlen(s);
+      return (aux(s, l));
 }
