2d1a0f58efc0f9d71b08586cb082856ba45ca7e4
Leidysalda <leidysalda1@gmail.com>
2019-10-10 00:34:42 +0000

    Number prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * is_prime_number2 - a function that prints string in reverse.
+ * @n: base
+ * @m: exponent
+ *Return: Always 0.
+ */
+
+int is_prime_number2(int n, int m)
+{
+	if ((n <= 1 || m != n) && (n % m == 0))
+		return (0);
+	if (m == n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_number2(n, m + 1));
+	}
+}
+
+/**
+ * is_prime_number - a function that prints string in reverse.
+ * @n: base
+ *
+ *Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime_number2(n, 2));
+}
