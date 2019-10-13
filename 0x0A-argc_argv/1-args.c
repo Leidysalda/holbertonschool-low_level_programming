816668d84e942dfc02955ec21aa190f9e9bb3a48
Leidysalda <leidysalda1@gmail.com>
2019-10-13 16:05:09 +0000

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Whats my name
+ * @argc: number
+ * @argv: number
+ * Return: Always 0.
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc-1);
+	return (0);
+}
