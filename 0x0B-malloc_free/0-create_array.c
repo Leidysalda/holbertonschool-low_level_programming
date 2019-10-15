a2c0929be6c4b3fe30ee8374639ce2e03b9c397f
Leidysalda <leidysalda1@gmail.com>
2019-10-15 23:25:06 +0000

    Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * *create_array - create a array
+ * @size: size array
+ * @c:
+ *
+ *Return: Always 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+	s = malloc((size+1) * sizeof(char));
+	if (s == NULL);
+	{
+		return (NULL);
+	}
+	i= 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,11 @@
+#ifndef PRINT_REV_H
+#define PRINT_REV_H
+
+
+char *argstostr(int ac, char **av);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+#endif
