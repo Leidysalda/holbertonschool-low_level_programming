13287b4d7f6183ab30bcf558cbfb92e954086b92
Leidysalda <leidysalda1@gmail.com>
2019-10-16 21:33:58 +0000

    Add First - of 3 point

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * **alloc_grid - two array
+ * @width: array
+ * @height: array
+ *
+ *Return: Always 0.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **pointer;
+	int sizew, sizeh, i;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	pointer = malloc(height * sizeof(int *));
+
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+
+	for (sizeh = 0; height > sizeh; sizeh++)
+	{
+		pointer[sizeh] = malloc(sizeof(int) * width);
+
+		if (pointer[sizeh] == NULL)
+		{
+			for (i = 0; i < sizeh; i++)
+			{
+				free(pointer[sizeh]);
+			}
+			free(pointer);
+			return (NULL);
+		}
+
+		for (sizew = 0; width > sizew; sizew++)
+		{
+			pointer[sizeh][sizew] = 0;
+		}
+	}
+	return (pointer);
+}
