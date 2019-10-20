fe1523d3a2123b2eab2aa49506c9ad6050f3d4db
Leidysalda <leidysalda1@gmail.com>
2019-10-20 21:36:10 +0000

    Add file holberton.h

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,54 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c)
+#endif
+
+#ifndef PRINT_ALPHABET_H
+#define PRINT_ALPHABET_H
+void print_alphabet(void);
+#endif
+
+#ifndef PRINT_ALPHABET_x10_H
+#define PRINT_ALPHABET_x10_H
+void print_alphabet_x10(void);
+#endif
+
+#ifndef _ISLOWER_H
+#define _ISLOWER_H
+int _islower(int c);
+#endif
+
+#ifndef _ISALPHA_H
+#define _ISALPHA_H
+int _isalpha(int c);
+#endif
+
+#ifndef _ABS_H
+#define _ABS_H
+int _abs(int);
+#endif
+
+#ifndef PRINT_LAST_DIGIT_H
+#define PRINT_LAST_DIGIT_H
+int print_last_digit(int);
+#endif
+
+#ifndef JACK_BAUER_H
+#define JACK_BAUER_H
+void jack_bauer(void);
+#endif
+
+#ifndef TIMES_TABLE_H
+#define TIMES_TABLE_H
+void times_table(void)
+#endif
+
+#ifndef ADD_H
+#define ADD_H
+int add(int, int);
+#endif
+
+#ifndef PRINT_TO_98_H
+#define PRINT_TO_98_H
+void print_to_98(int n);
+#endif
