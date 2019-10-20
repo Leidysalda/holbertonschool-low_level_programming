028ec538de76a505d03169a8d93732f8a5ef2a2c
Leidysalda <leidysalda1@gmail.com>
2019-10-20 22:33:04 +0000

    add change of type of variable

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
+
 /**
  * _abs - print absolute value of an integer
- * @r:The caracter to print
+ * @r: The caracter to print
  *
- *Return: 0n success 1.
+ * Return: 0n success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _abs(int r)
+unsigned int _abs(unsigned int r)
 {
-if (r >= 0)
-_putchar(r);
-else
-r = r * -1;
-return (r);
+	if (r >= 0)
+	{
+		_putchar(r);
+	}
+	else
+	{
+		r = r * -1;
+	}
+	return (r);
 }
