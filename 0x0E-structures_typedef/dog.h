510a7ed36f906df9cd919ed31c88e04ff4e33603
Leidysalda <leidysalda1@gmail.com>
2019-10-21 23:58:18 +0000

    change name of structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * Description: Struct Dog
  */
 
-struct dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
