1349cf3d0dc76a7b25b6a14dc57eb7edb93c0705
Leidysalda <leidysalda1@gmail.com>
2019-10-21 23:52:09 +0000

    up 2 exercise

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - Initial data
  * @name: Name
@@ -16,9 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif
 
-#ifndef INIT_DOG_H
-#define INIT_DOG_H
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
