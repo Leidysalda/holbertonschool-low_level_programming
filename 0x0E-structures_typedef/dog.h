d578abac282c9a30adf166e93ba134ea6fc6cc29
Leidysalda <leidysalda1@gmail.com>
2019-10-21 19:51:18 +0000

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+void _putchar(char c);
+#endif
+
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+
+struct dog /*DOG_H */
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
+#ifndef PRINT_ALPHABET_H
+#define PRINT_ALPHABET_H
+void print_alphabet(void);
+#endif
