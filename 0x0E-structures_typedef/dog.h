8eeb3458e66c6088315b300b96470b51e4908118
Leidysalda <leidysalda1@gmail.com>
2019-10-21 20:02:34 +0000

    correction dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,19 @@
-#ifndef _PUTCHAR_H
-#define _PUTCHAR_H
-void _putchar(char c);
-#endif
-
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - struct dog
- * @name: name
- * @age: age
- * @owner: owner
+ * struct dog - Initial data
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
  *
+ * Description: Struct Dog
  */
 
-struct dog /*DOG_H */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-#endif
-
-#ifndef PRINT_ALPHABET_H
-#define PRINT_ALPHABET_H
-void print_alphabet(void);
 #endif
