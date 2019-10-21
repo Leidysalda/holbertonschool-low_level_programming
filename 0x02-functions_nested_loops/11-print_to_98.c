8a82b5f9bfe9e1b3ab0076f4d8b0763438d23ca2
Leidysalda <leidysalda1@gmail.com>
2019-10-21 05:10:36 +0000

    add exercise

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print serial number at 98
+ * @num: number limit
+ *
+ * Return: 0n success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_to_98(int num)
+{
+	if (num < 98)
+	{
+		while (num <= 98)
+		{
+			printf("%d", num);
+			if (num != 98)
+			{
+				printf(", ");
+			}
+			num++;
+		}
+	}
+	else if (num > 98)
+	{
+		while (num >= 98)
+		{
+			printf("%d", num);
+			if (num != 98)
+			{
+				printf(", ");
+			}
+			num--;
+		}
+	}
+	else
+		printf("98");
+
+	printf('\n');
+
+	return (0);
+}
