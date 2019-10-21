907bb7c45a8f6b4790c1cb22d350d35e4e2935f7
Leidysalda <leidysalda1@gmail.com>
2019-10-21 01:39:11 +0000

    Push exercise 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,27 @@
 #include "holberton.h"
+
 /**
- * print_last_digit - print last digit of a number
- * @r:The caracter to print
+ * print_last_digit - print absolute value of an integer
+ * @r: The caracter to print
  *
- *Return: 0n success 1.
+ * Return: 0n success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int print_last_digit(int r)
 {
-r = (r % 10);
-return(r);
+	int res;
+
+	res = r % 10;
+
+	if (res > 0 && res < 9)
+	{
+		_putchar(res + '0');
+		return (res);
+	}
+	else
+	{
+		res = res * -1;
+		_putchar(res + '0');
+		return (res);
+	}
 }
