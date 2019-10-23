bf51edc8e2ab7ea248981f1d2d852faf281b8378
Leidysalda <leidysalda1@gmail.com>
2019-10-23 01:52:15 +0000

    Change exercise

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,42 +1,31 @@
 #include "holberton.h"
 /**
- * print_line - write a line
- *@n :
-<<<<<<< HEAD
- *: integer
- *Return: 0n success 1.
+ * print_diagonal - write a diagonal
+ * @n :
+ *
+ * Return: 0n success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void print_line(int n)
-{
-int i;
-i = 0;
-while (i < n)
-{
-_putchar (95);
-i++;
-}
-_putchar ('\n');
-}
-=======
- * integer
- *Return: 0n success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+
 void print_diagonal(int n)
 {
-int i, j;
-i = 1;
-for (i = 1; (i <= n); i++)
-{
-if (n <= 0)
-{
-_putchar('\n');
+	int i, j;
+
+	j = 0;
+	if (0 < n)
+	{
+		for (i = 0; i < n; i++)
+		{
+			while (i > 0 && j < i)
+			{
+				_putchar (' ');
+				j++;
+			}
+			j = 0;
+			_putchar('\\');
+			_putchar ('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
-for (j = 1; j <= i ; j++)
-_putchar (' ');
-_putchar (92);
-_putchar ('\n');
-}
-}
->>>>>>> e54f2edea3115b1a64769758305ee74bc8402bd3
