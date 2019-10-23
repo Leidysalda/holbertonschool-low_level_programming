554cfbd897abc43280b4b0c849293721644e5552
Leidysalda <leidysalda1@gmail.com>
2019-10-23 22:19:39 +0000

    [Fix]: change 2 exercise

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,24 +11,22 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-        int i;
+	int i;
 
-        if (cmp != NULL && array != NULL)
-        {
+	if (cmp != NULL && array != 0 && size != 0)
+	{
 		for (i = 0; i < size; i++)
-                {
-			if (!(size <= 0 && size != i))
+		{
+			if (cmp(array[i]) != 0)
 			{
-				if (cmp(array[i]) != 0)
-				{
-					return (i);
-				}
-			}
-			else
-			{
-				return(-1);
+				return (i);
 			}
 		}
-        }
+	}
+	else
+	{
+		return (-1);
+	}
+
 	return (0);
 }
