2a577b69078474713bb1d9d4189348c8844cd7e9
Leidysalda <leidysalda1@gmail.com>
2019-10-23 17:38:36 +0000

    [Feature]: New files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - print name
+ * @name: name
+ * @f: function
+ *
+ *Return: Always 0.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
