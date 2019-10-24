79ff8b8cd940ec4c28a7e73586e7918bbd3c1f4e
Leidysalda <leidysalda1@gmail.com>
2019-10-24 22:23:26 +0000

    Add files

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * get_op_func - array of operations
+ * @s: sing operator
+ *
+ * Return: 0n success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] =
+		{
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}
+		};
+	int i;
+
+	i = 0;
+
+	while (*ops[i].op != s[0] && i < 4)
+	{
+		i++;
+	}
+	return (ops[i].f);
+}
