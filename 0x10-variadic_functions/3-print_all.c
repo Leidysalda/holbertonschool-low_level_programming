3dfd42ad69712f670d9686fd718f0ecb871ceb99
Leidysalda <leidysalda1@gmail.com>
2019-10-27 03:06:25 +0000

    [Feature]: Add file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,72 +2,54 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * print_all - print all numbers or caracters.
- * @format: special caracter for separate
- * @type: type option
- * @f: function
+ * type_ch - print type of characters
+ * @args: instance of va_list
  *
  *Return: Always 0.
  */
-typedef struct format
-{
-        char *type;
-        void (*f)();
-} format_t;
-
-/**
- * type_ch - print all numbers or caracters.
- * @format: special caracter for separate
- *
- *Return: Always 0.
- */
-
 void type_ch(va_list args)
 {
-        printf("%c", va_arg(args, int));
+	printf("%c", va_arg(args, int));
 }
 
 /**
- * print_all - print all numbers or caracters.
- * @format: special caracter for separate
+ * type_in - print type of integer.
+ * @args: instance ofva_lis
  *
  *Return: Always 0.
  */
-
 void type_in(va_list args)
 {
-        printf("%d", va_arg(args, int));
+	printf("%d", va_arg(args, int));
 }
 
 /**
- * print_all - print all numbers or caracters.
- * @format: special caracter for separate
+ * type_fl - print type of float.
+ * @args: instance of va_lsit
  *
  *Return: Always 0.
  */
-
 void type_fl(va_list args)
 {
-        printf("%f", va_arg(args, double));
+printf("%f", va_arg(args, double));
 }
 
 /**
- * print_all - print all numbers or caracters.
- * @format: special caracter for separate
+ * type_st - print type of string
+ * @args: instance of va_list
  *
  *Return: Always 0.
  */
-
 void type_st(va_list args)
 {
-        char *s = va_arg(args, char *);
+	char *s = va_arg(args, char *);
 
-        if (s != NULL)
-        {
-                printf("%s", s);
-        }
-        else
-                printf("(nil)");
+	if (s != NULL)
+	{
+		printf("%s", s);
+	}
+	else
+		printf("(nil)");
 }
 
 /**
@@ -76,46 +58,39 @@ void type_st(va_list args)
  *
  *Return: Always 0.
  */
-
 void print_all(const char * const format, ...)
 {
-  
-        va_list args;
-        unsigned int j, i;
-
+	va_list args;
+	unsigned int j, i;
 	format_t selector[] = {
-                {"c", type_ch},
-                {"i", type_in},
-                {"f", type_fl},
-                {"s", type_st},
-                {NULL, NULL}
-        };
+		{"c", type_ch},
+		{"i", type_in},
+		{"f", type_fl},
+		{"s", type_st},
+		{NULL, NULL}
+	};
 
-        va_start(args, format);
+	va_start(args, format);
+	j = 0;
 
-        j = 0;
-        while (format && format[j])
+	while (format && format[j])
 	{
 		i = 0;
 		while (selector[i].type != NULL)
 		{
 			if (*(selector[i].type) == format[j])
 			{
-            
 				((selector[i].f)(args));
-            
-				if (format[j+1] != '\0')
+				if (format[j + 1] != '\0')
 				{
 					printf(", ");
 				}
 				break;
 			}
-			i++;    
+			i++;
 		}
 		j++;
 	}
-        va_end(args);
-        printf("\n");
-   
-	return;
+	va_end(args);
+	printf("\n");
 }
