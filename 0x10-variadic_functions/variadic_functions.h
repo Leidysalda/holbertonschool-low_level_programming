093f8130ceda92cc06ca5a56cb3a0911184640b3
Leidysalda <leidysalda1@gmail.com>
2019-10-28 04:22:25 +0000

    Correction 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
+
 /**
  * struct format - Struct format of data
  *
@@ -14,8 +16,10 @@ typedef struct format
 	void (*f)(va_list args);
 } format_t;
 
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
