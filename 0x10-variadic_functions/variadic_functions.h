fc1ece6c5612a916efa442c8bcc5055e5b351798
Leidysalda <leidysalda1@gmail.com>
2019-10-28 04:18:45 +0000

    Correction 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,15 +11,9 @@
 typedef struct format
 {
 	char *type;
-	void (*f)();
+	void (*f)(va_list args);
 } format_t;
 
-void type_ch(va_list args);
-void type_in(va_list args);
-void type_fl(va_list args);
-void type_st(va_list args);
-void get_type_func(char s, va_list args);
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
