c802ae1cbcebfa90ae784c2d9d1866a37552e57f
Leidysalda <leidysalda1@gmail.com>
2019-10-29 02:48:13 +0000

    Add change 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,22 +7,19 @@
  * @separator: special caracter for separate
  * @n: number
  *
- * Return: Always 0.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list argumentos;
+	va_list argument;
 
-	va_start(argumentos, n);
-
+	va_start(argument, n);
 	for  (i = 0 ; i < n ;  i++)
 	{
-		printf("%d", (va_arg(argumentos, unsigned int)));
-		if (i != (n - 1) && separator != NULL)
+		printf("%d", va_arg(argument, int));
+		if (i < (n - 1) && separator != NULL)
 			printf("%s ", separator);
 	}
 	printf("\n");
-	va_end(argumentos);
+	va_end(argument);
 }
