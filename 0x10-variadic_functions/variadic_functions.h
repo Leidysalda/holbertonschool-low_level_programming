78bb2981c1f671453669d6d01b598384966bb146
Leidysalda <leidysalda1@gmail.com>
2019-10-29 02:09:28 +0000

    Add change

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#include <stdarg.h>
-
-/**
- * struct format - Struct format of data
- *
- * @type: Type of data
- * @f: The function associated
- */
-
-typedef struct format
-{
-	char *type;
-	void (*f)(va_list args);
-} format_t;
-
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
