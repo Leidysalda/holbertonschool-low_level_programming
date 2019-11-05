1c4ecc1a08c050a6b4e5521e715c21c0af070e4b
Leidysalda <leidysalda1@gmail.com>
2019-11-05 17:27:54 +0000

    [Feature]: Add file 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * free_listint - free list.
+ * @head: initial node
+ *
+ * Return: void.
+ */
+void free_listint(listint_t *head)
+{
+	if(head->next)
+		free_listint(head->next);
+	free (head);
+}
