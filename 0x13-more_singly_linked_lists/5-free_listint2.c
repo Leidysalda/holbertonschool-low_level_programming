b89d3679fa8fd267e9126a0cde8751ff595f6210
Leidysalda <leidysalda1@gmail.com>
2019-11-05 18:14:02 +0000

    [Feature]: Add 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t.
+ * @head: initial node
+ *
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	tmp = *head;
+
+	while (tmp != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
