b0ee96878d735ad2a91063783b2cf5829d16be8c
Leidysalda <leidysalda1@gmail.com>
2019-11-08 20:37:04 +0000

    [Fix]: add Null comparation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,9 +9,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0;
 
+	if (*b != '\0')
+	{}
 	while (*b)
 	{
-		if ((*b != '0' && *b != '1') && (!b))
+		if (*b != '0' && *b != '1')
 			return (0);
 
 		i <<= 1;
