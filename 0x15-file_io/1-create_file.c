1ea6ad9c4a631a3d6020c22d78c76057a1792153
Leidysalda <leidysalda1@gmail.com>
2019-11-14 01:31:52 +0000

    Add corrections file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,11 +12,11 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
+	int fd;
 	ssize_t new;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fd = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	new = write(fd, text_content, i);
+	new = write(fd, text_content, sizeof(text_content));
 	close(fd);
 	if (new == -1)
 		return (-1);
