547a0708f14fe51da93338986e3dd38fa16d9ab3
Leidysalda <leidysalda1@gmail.com>
2019-11-14 01:20:32 +0000

    [Feature]: Add file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * create_file - function that creates a file.
+ *
+ * @filename: name of the file which you want to create.
+ * @text_content: indicates permissions of new file.
+ *
+ * Return:  0, 1, 2 or -1 upon failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+	ssize_t new;
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close (fd);
+		return (-1);
+	}
+
+	new = write (fd, text_content, i);
+		close(fd);
+	if (new == -1)
+		return (-1);
+
+	return (1);
+}
