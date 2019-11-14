c1dcdee4fcce26536b2a5c3337f2e68a112f5b22
Leidysalda <leidysalda1@gmail.com>
2019-11-14 02:11:22 +0000

    Add correction flags

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND,
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND,
 		  S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
