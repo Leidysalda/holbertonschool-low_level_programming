025c73b9f9fc38b9fc55c7a31ff01be267e6b228
Leidysalda <leidysalda1@gmail.com>
2020-01-19 21:11:42 +0000

    Add free tmp

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
 /**
  * hash_table_create - function that creates a hash table.
@@ -6,17 +9,28 @@
  *
  * Return: pointer to the newly created hash table.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *tmp = malloc(sizeof(hash_node_t));
+	hash_table_t *tmp;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
+
+	tmp = malloc(sizeof(hash_node_t));
 	if (tmp == NULL)
 		return (NULL);
 
+	tmp->array = malloc(sizeof(hash_node_t *) * size);
+	if (tmp == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
+	tmp->size = size;
+
 	for (i = 0; i < size; i++)
-		tmp[i].array = NULL;
+		tmp->array[i] = NULL;
 
 	return (tmp);
 }
