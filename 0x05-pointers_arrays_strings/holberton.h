0a503d33742be2eaf554f85bebd977dc509d4aa5
Leidysalda <leidysalda1@gmail.com>
2020-04-23 16:19:12 +0000

    Fix file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,7 +1,7 @@
 #ifndef PRINT_REV_H
 #define PRINT_REV_H
 
-char _strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts2(char *str);
 void rev_string(char *s);
