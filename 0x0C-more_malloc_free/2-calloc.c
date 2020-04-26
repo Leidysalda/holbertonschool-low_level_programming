445d5c268e07dabe9cf1fd4e49b8e6348e9bed74
Leidysalda <leidysalda1@gmail.com>
2020-04-26 19:17:35 +0000

    Fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,14 +9,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	 char *new = malloc(nmemb * size);
-	 unsigned int i;
+	char *new;
+	unsigned int i;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
+	new = malloc(nmemb * size);
 	if (new == NULL)
 		return (NULL);
+
 	for (i = 0; i < (nmemb * size); i++)
 	{
 		new[i] = 0;
