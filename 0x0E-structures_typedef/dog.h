55fa22952a3c0238fd65efbdb60950e835f3225e
Leidysalda <leidysalda1@gmail.com>
2020-04-27 02:23:40 +0000

    Up library

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+
 /**
  * struct dog - Initial data
  * @name: Name
@@ -17,6 +19,12 @@ struct dog
 };
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int strl(char *s);
+char *strc(char *des, char *src);
+
 #endif
