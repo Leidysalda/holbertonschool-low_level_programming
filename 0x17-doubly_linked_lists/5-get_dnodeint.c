a2cc390bbb378b503e84ea19da9551551c2ac222
Leidysalda <leidysalda1@gmail.com>
2020-05-04 01:43:29 +0000

    [Fix] file

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,9 +13,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *find = head;
 	unsigned int i = 0;
 
-	if (head == NULL)
-		return (NULL);
-
 	while (index > i)
 	{
 		find = head->next;
