9e7b11be663cadd09d92cf1149f73d01ea6f713f
Leidysalda <leidysalda1@gmail.com>
2020-05-30 00:31:35 +0000

    Add files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function that creates a hash table
+ *
+ * @size: size
+ *
+ * Return: size
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tmp;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	tmp = malloc(sizeof(hash_node_t));
+	if (tmp == NULL)
+		return (NULL);
+
+	tmp->array = malloc(sizeof(hash_node_t *) * size);
+	if (tmp == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
+	tmp->size = size;
+
+	for (i = 0; i < size; i++)
+		tmp->array[i] = NULL;
+
+	return (tmp);
+}
